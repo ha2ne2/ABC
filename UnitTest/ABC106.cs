5e38ba6e52aa7ec4f95873d19d882383fe070003
ha2ne2 <hatsunetsu7@gmail.com>
2020-02-01 14:12:09 +0900

    ABC106を解いた。パフォ1050。

---
+++ UnitTest/ABC106.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+/// <summary>
+/// Usage
+/// 1.テスト対象プロジェクトを参照に追加
+/// 2.namespaceをテスト対象と同じにする
+/// 3.テストコード貼り付け
+/// </summary>
+namespace ABC106
+{
+    [TestClass]
+    public class DTest
+    {
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.D();
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+
+        [TestMethod]
+        public void 入力例_1()
+        {
+            string input =
+@"2 3 1
+1 1
+1 2
+2 2
+1 2";
+            string output =
+@"3";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_2()
+        {
+            string input =
+@"10 3 2
+1 5
+2 8
+7 10
+1 7
+3 10";
+            string output =
+@"1
+1";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_3()
+        {
+            string input =
+@"10 10 10
+1 6
+2 9
+4 5
+4 7
+4 7
+5 8
+6 6
+6 7
+7 9
+10 10
+1 8
+1 9
+1 10
+2 8
+2 9
+2 10
+3 8
+3 9
+3 10
+1 10";
+            string output =
+@"7
+9
+10
+6
+8
+9
+6
+7
+8
+10";
+
+            AssertIO(input, output);
+        }
+
+    }
+
+    [TestClass]
+    public class CTest
+    {
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.C();
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+
+        [TestMethod]
+        public void 入力例_1()
+        {
+            string input =
+@"1214
+4";
+            string output =
+@"2";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_2()
+        {
+            string input =
+@"3
+157";
+            string output =
+@"3";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_3()
+        {
+            string input =
+@"299792458
+9460730472580800";
+            string output =
+@"2";
+
+            AssertIO(input, output);
+        }
+
+    }
+
+    [TestClass]
+    public class BTest
+    {
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.B();
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+
+        [TestMethod]
+        public void 入力例_1()
+        {
+            string input =
+@"105";
+            string output =
+@"1";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_2()
+        {
+            string input =
+@"7";
+            string output =
+@"0";
+
+            AssertIO(input, output);
+        }
+
+    }
+
+    [TestClass]
+    public class ATest
+    {
+        private void AssertIO(string input, string output)
+        {
+            StringReader reader = new StringReader(input);
+            Console.SetIn(reader);
+
+            StringWriter writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Program.A();
+
+            Assert.AreEqual(output + Environment.NewLine, writer.ToString());
+        }
+
+        [TestMethod]
+        public void 入力例_1()
+        {
+            string input =
+@"2 2";
+            string output =
+@"1";
+
+            AssertIO(input, output);
+        }
+
+        [TestMethod]
+        public void 入力例_2()
+        {
+            string input =
+@"5 7";
+            string output =
+@"24";
+
+            AssertIO(input, output);
+        }
+
+    }
+
+
+}
